889e28d2c3923c19031847aae4527e3bd7405862
hahabob <orisor101@gmail.com>
2025-06-05 16:01:25 +0300

    deleted and moved to utils

--- scripts/modules/navDropdown.js
+++
@@ -1,7 +0,0 @@
-// ===== Dropdown Menu Toggle =====
-const toggleButton = document.getElementById("menu-toggle");
-const menuLinks = document.getElementById("menu-links");
-
-toggleButton.addEventListener("click", () => {
-  menuLinks.classList.toggle("open");
-});
